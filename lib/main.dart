6f6b98c9a35eb73abb15dcd1f61cd33c39e7290c
Diego <diegocleir@gmail.com>
2022-10-07 13:08:44 -0300

    melhorias

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class _HomeState extends State<Home> {
     final caminhoBancoDados = await getDatabasesPath();
     final localBancoDados = join(caminhoBancoDados, "banco.db");
 
-    var retorno = await openDatabase(
+    var bd = await openDatabase(
       localBancoDados,
       version: 1,
       onCreate: (db, dbVersaoRecente){
@@ -31,7 +31,91 @@ class _HomeState extends State<Home> {
       }
     );
 
-    print("aberto: " + retorno.isOpen.toString() );
+    return bd;
+    //print("aberto: " + bd.isOpen.toString() );
+
+  }
+
+  _salvar() async {
+
+    Database bd = await _recuperarBancoDados();
+
+    Map<String, dynamic> dadosUsuario = {
+      "nome": "Justina Lamistiano",
+      "idade": 65
+    };
+    int id = await bd.insert("usuarios", dadosUsuario);
+    print("Salvo: ${id.toString()} ");
+  }
+
+  _listarUsuarios() async {
+    Database bd = await _recuperarBancoDados();
+
+    String query = "select * from usuarios order by UPPER(nome) ASC";
+    List usuarios = await bd.rawQuery(query);
+
+    for( var usuario in usuarios ){
+      print(
+        "item id: " + usuario["id"].toString() +
+          " nome: " + usuario["nome"] +
+            " idade: " + usuario["idade"].toString()
+      );
+    }
+
+    //print("usuarios: " + usuarios.toString() );
+
+  }
+
+  _listarUsuarioPorId(int id) async {
+
+    Database bd = await _recuperarBancoDados();
+
+    List usuarios = await bd.query(
+        "usuarios",
+        where: "id = ?",
+        whereArgs: [id]
+    );
+
+    for( var usuario in usuarios ){
+      print(
+          "item id: " + usuario["id"].toString() +
+              " nome: " + usuario["nome"] +
+              " idade: " + usuario["idade"].toString()
+      );
+    }
+
+  }
+
+  _excluirUsuario(int id) async {
+
+    Database bd = await _recuperarBancoDados();
+
+    int retorno = await bd.delete(
+        "usuarios",
+        where: "id = ?",
+        whereArgs: [id]
+    );
+
+    print("item qtde removida: " + retorno.toString());
+
+  }
+
+  _atualizarUsuario(int id) async {
+
+    Database bd = await _recuperarBancoDados();
+
+    Map<String, dynamic> dadosUsuario = {
+      "nome": "Diego Alves alterado",
+      "idade": 1
+    };
+    int retorno = await bd.update(
+        "usuarios",
+        where: 'id=?',
+        whereArgs: [id],
+        dadosUsuario
+    );
+
+    print("item qtde atualizada: " + retorno.toString());
 
   }
 
@@ -39,7 +123,11 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
 
-    _recuperarBancoDados();
+    _salvar();
+    //_excluirUsuario(2);
+    //_atualizarUsuario(1);
+    _listarUsuarios();
+    //_listarUsuarioPorId(1);
 
     return Container();
   }
